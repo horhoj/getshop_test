82f5baf3e77988dbfe552957b9c90397ded16b58
Vasiliy Zadoya <cool29@yandex.ru>
2023-10-22 13:53:51 +0500

    refactoring: removed unnecessary packages and components

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
 import '~/styles/index.scss';
-import { store } from './store';
 import { ReactStrictModeWrapper } from './ui/ReactStrictModeWrapper';
 import { App } from './app';
 import { IS_REACT_STRICT_MODE } from '~/config/app';
@@ -12,9 +10,7 @@ if (container) {
   const root = createRoot(container);
   root.render(
     <ReactStrictModeWrapper isStrictModeEnable={IS_REACT_STRICT_MODE}>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
     </ReactStrictModeWrapper>,
   );
 }
--- src/store/store.ts
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { microSiteSlice } from '~/features/microSite/store/microSiteSlice';
-
-export const store = configureStore({
-  devTools: true,
-  reducer: {
-    microSite: microSiteSlice.reducer,
-  },
-});
